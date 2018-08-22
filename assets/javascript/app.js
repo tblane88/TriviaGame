ec92bc4a2a87f5913889032aac78ee1b6b2d6a87
tblane <tblane88@gmail.com>
2018-08-21 22:24:07 -0400

    Added an array of objects with questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+
+var choices = [{
+                question: "In 'The Alliance' episode, Michael is asked by Oscar to donate to his nephew's walkathon for a charity. How much money does Michael donate, not realizing that the donation is per mile and not a flat amount?",
+                answerOne: "$25.00",
+                answerTwo: "$5.00",
+                answerThree: "$15.00",
+                answerFour: "$40.00"
+               },
+               {
+                question: "What is Michael Scott's middle name?",
+                answerOne: "Gary",
+                answerTwo: "Hank",
+                answerThree: "Phillip",
+                answerFour: "Jason"
+
+                }, 
+                {
+                question: "Which television series revolves around a concierge doctor making house calls in the Hampstons?",
+                answerOne: "Royal Pains",
+                answerTwo: "Dr.Quinn Medicine Woman",
+                answerThree: "Doogie Howser, M.D.",
+                answerFour: "Scrubs"
+
+                },   
+                {
+                question: "Who plays the character Dexter on the tv series 'Dexter'?",
+                answerOne: "Michael C. Hall",
+                answerTwo: "James Roday",
+                answerThree: "Phillip Seymour Hoffman",
+                answerFour: "Ed O'Neil"
+
+                },
+                {
+                question: "What type of dog is Scooby Doo from the cartoon series, 'Scooby Doo'",
+                answerOne: "Great Dane",
+                answerTwo: "Poodle",
+                answerThree: "Australian Mastiff",
+                answerFour: "Labrador Retreiver"
+
+                },
+                {
+                question: "What is Chandler's last name in the show 'Friends'?",
+                answerOne: "Bing",
+                answerTwo: "Anderson",
+                answerThree: "Paul",
+                answerFour: "Gellar"
+
+                },     
+                {
+                question: "What hospital does the show 'Grey's Anatomy' take place in?",
+                answerOne: "Seatle Grace Hospital",
+                answerTwo: "Miami Dade Medical Center",
+                answerThree: "Johns Hopkins",
+                answerFour: "New York General"
+
+                }, 
+                {
+                question: "What are the names of the two Muppet characters that heckle the rest of the cast from the balcony?",
+                answerOne: "Statler and Waldorf",
+                answerTwo: "William and Johan",
+                answerThree: "Bert and Ernie",
+                answerFour: "Cobblepot and Oswald"
+
+                },
+                {
+                question: "What was Mickey Mouse's name before it was changed to Mickey?",
+                answerOne: "Mortimer",
+                answerTwo: "Maxwell",
+                answerThree: "Willie",
+                answerFour: "Preston"
+
+                },  
+                {
+                question: "In the tv show The Simpsons, what is the name of Ned Flander's two childen?",
+                answerOne: "Rod and Todd",
+                answerTwo: "Bart and Lisa",
+                answerThree: "Sam and Max",
+                answerFour: "Dustin and Dart"
+
+                }];
+
+
+
+
+});
